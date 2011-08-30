50ced8045119ecfd3ccb49d6404d88d68ed2ce78
Sascha G <sushi.genji@hotmail.de>
2011-08-30 18:19:21 +0200

    Such link hinzugefügt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
 
+    respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
